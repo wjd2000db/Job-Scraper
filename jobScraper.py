a09329ae47bc8dc2433840ce783f520ba69fe9d6
Yujin <user@Yujins-MacBookPro.local>
2023-05-16 15:57:32 -0400

    save job posting info to dictionary

--- jobScraper.py
+++ jobScraper.py
@@ -8,6 +8,7 @@ response=get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
+    results = []
     soup = BeautifulSoup(response.text, "html.parser")
     jobs=soup.find_all('section',class_="jobs")
     for job_section in jobs:
@@ -16,4 +17,16 @@ else:
         for post in job_posts:
             anchors = post.find_all('a')
             anchor = anchors[1]
-            print(anchor['href'])
+            link = anchor['href']
+            company, kind, region = anchor.find_all('span', class_= "company")
+            title = anchor.find('span', class_='title')
+            job_data = {
+                'link': f"https://weworkremotely.com/{link}",
+                'company': company.string,
+                'region': region.string,
+                'position': title.string
+            }
+            results.append(job_data)
+    for result in results:
+        print(result)
+        print("/////")
