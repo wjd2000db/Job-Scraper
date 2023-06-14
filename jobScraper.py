8b8b771ec9bca2883054747a5c9920e31b446d3f
Yujin <user@Yujins-MacBookPro.local>
2023-06-14 15:03:09 -0400

    get job data and add to the list

--- jobScraper.py
+++ jobScraper.py
@@ -1,32 +1,45 @@
 from requests import get
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term ="python"
+options = Options()
 
-response=get(f"{base_url}{search_term}")
-if response.status_code != 200:
-    print("Can't request website")
+options.add_experimental_option("detach", True)
+# browser = webdriver.Chrome(options=options)
+
+# browser.get("https://ca.indeed.com/jobs?q=software+developer+intern&vjk=2a587d68efa44b7d")
+
+url = 'https://ca.indeed.com/jobs?q=software+developer+intern&vjk=2a587d68efa44b7d'
+
+browser = webdriver.Chrome(options=options,executable_path="/Users/usr/Projects/Selenium/chromedriver_mac64/chromedriver")
+browser.get(url)
+
+
+
+if "Error" in browser.title:
+    print("An error occurred while loading the page.")
 else:
     results = []
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs=soup.find_all('section',class_="jobs")
-    for job_section in jobs:
-        job_posts = job_section.find_all('li')
-        job_posts.pop(-1)                   # remove last class "view-all" 
-        for post in job_posts:
-            anchors = post.find_all('a')
-            anchor = anchors[1]
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    job_list = (soup.find("ul", class_="jobsearch-ResultsList"))
+    jobs = job_list.find_all('li', recursive=False)
+    for job in jobs:
+        zone = job.find("div", class_="mosaic-zone")
+        if zone == None:
+            anchor = job.select_one("h2 a")
+            title = anchor['aria-label']
             link = anchor['href']
-            company, kind, region = anchor.find_all('span', class_= "company")
-            title = anchor.find('span', class_='title')
+            company = job.find("span",class_="companyName")
+            location = job.find("div",class_="companyLocation")
             job_data = {
-                'link': f"https://weworkremotely.com/{link}",
+                'link': f"https://ca.indeed.com/\{link}",
                 'company': company.string,
-                'region': region.string,
-                'position': title.string
+                'location': location.string,
+                'position': title
             }
             results.append(job_data)
     for result in results:
-        print(result)
-        print("/////")
+        print(result,"\n/////\n")
+
+
